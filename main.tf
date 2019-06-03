6202a9de453cc0a0e1157484a46ccd349d2ac2f4
jessiemoss <jmoss@salucro.com>
2019-06-03 09:38:06 -0700

    Add Inputs for Access Logging

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ locals {
 }
 
 resource "aws_s3_bucket" "this" {
-  bucket        = "${lower(local.id)}"
-  acl           = "${var.acl}"
-  tags          = "${var.tags}"
+  bucket = "${lower(local.id)}"
+  acl    = "${var.acl}"
+  tags   = "${var.tags}"
 
   cors_rule {
     allowed_headers = "${var.cors_allowed_headers}"
@@ -14,6 +14,8 @@ resource "aws_s3_bucket" "this" {
     expose_headers  = "${var.cors_expose_headers}"
     max_age_seconds = "${var.cors_max_age_seconds}"
   }
+
+  logging = "${var.logging}"
 
   versioning {
     enabled = "${var.versioning_enabled}"
@@ -25,39 +27,39 @@ resource "aws_s3_bucket" "this" {
 }
 
 resource "aws_s3_bucket_policy" "access_identity" {
-  count = "${var.access_identity ? 1 : 0}"
+  count  = "${var.access_identity ? 1 : 0}"
   bucket = "${aws_s3_bucket.this.id}"
   policy = "${data.template_file.access_identity.rendered}"
 }
 
 resource "aws_s3_bucket_policy" "public" {
-  count = "${var.acl == "public-read" ? 1 : 0}"
+  count  = "${var.acl == "public-read" ? 1 : 0}"
   bucket = "${aws_s3_bucket.this.id}"
   policy = "${data.template_file.public.rendered}"
 }
 
 resource "aws_iam_policy" "read" {
-  count = "${length(var.read_roles) > 0 ? 1 : 0}"
-  name   = "${local.id}-S3-Read"
+  count       = "${length(var.read_roles) > 0 ? 1 : 0}"
+  name        = "${local.id}-S3-Read"
   description = "${var.description} Read"
-  policy = "${data.aws_iam_policy_document.read.json}"
+  policy      = "${data.aws_iam_policy_document.read.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "read" {
-  count = "${length(var.read_roles)}"
-  role = "${element(var.read_roles, count.index)}"
+  count      = "${length(var.read_roles)}"
+  role       = "${element(var.read_roles, count.index)}"
   policy_arn = "${aws_iam_policy.read.arn}"
 }
 
 resource "aws_iam_policy" "write" {
-  count = "${length(var.write_roles) > 0 ? 1 : 0}"
-  name   = "${local.id}-S3-Write"
+  count       = "${length(var.write_roles) > 0 ? 1 : 0}"
+  name        = "${local.id}-S3-Write"
   description = "${var.description} Write"
-  policy = "${data.aws_iam_policy_document.write.json}"
+  policy      = "${data.aws_iam_policy_document.write.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "write" {
-  count = "${length(var.write_roles)}"
-  role = "${element(var.write_roles, count.index)}"
+  count      = "${length(var.write_roles)}"
+  role       = "${element(var.write_roles, count.index)}"
   policy_arn = "${aws_iam_policy.write.arn}"
 }
