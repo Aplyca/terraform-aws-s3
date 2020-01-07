4cc6a87d7e9efc0bf9aa2e508dc425f3d312aa97
Mauricio Sánchez <msanchez@aplyca.com>
2020-01-06 22:59:19 -0500

    Improve upgade to Terraform 0.12

--- main.tf
+++ main.tf
@@ -7,16 +7,16 @@ locals {
 }
 
 resource "aws_s3_bucket" "this" {
-  bucket = "${lower(local.id)}"
-  acl    = "${var.acl}"
-  tags   = "${var.tags}"
+  bucket = lower(local.id)
+  acl    = var.acl
+  tags   = var.tags
 
   cors_rule {
-    allowed_headers = "${var.cors_allowed_headers}"
-    allowed_methods = "${var.cors_allowed_methods}"
-    allowed_origins = "${var.cors_allowed_origins}"
-    expose_headers  = "${var.cors_expose_headers}"
-    max_age_seconds = "${var.cors_max_age_seconds}"
+    allowed_headers = var.cors_allowed_headers
+    allowed_methods = var.cors_allowed_methods
+    allowed_origins = var.cors_allowed_origins
+    expose_headers  = var.cors_expose_headers
+    max_age_seconds = var.cors_max_age_seconds
   }
 
   dynamic "logging" {
@@ -28,7 +28,7 @@ resource "aws_s3_bucket" "this" {
   }
 
   versioning {
-    enabled = "${var.versioning_enabled}"
+    enabled = var.versioning_enabled
   }
 
   dynamic "website" {
@@ -87,39 +87,39 @@ resource "aws_s3_bucket" "this" {
 }
 
 resource "aws_s3_bucket_policy" "access_identity" {
-  count  = "${var.access_identity ? 1 : 0}"
-  bucket = "${aws_s3_bucket.this.id}"
-  policy = "${element(data.template_file.access_identity.*.rendered, 0)}"
+  count  = var.access_identity ? 1 : 0
+  bucket = aws_s3_bucket.this.id
+  policy = element(data.template_file.access_identity.*.rendered, 0)
 }
 
 resource "aws_s3_bucket_policy" "public" {
-  count  = "${var.acl == "public-read" ? 1 : 0}"
-  bucket = "${aws_s3_bucket.this.id}"
-  policy = "${element(data.template_file.public.*.rendered, 0)}"
+  count  = var.acl == "public-read" ? 1 : 0
+  bucket = aws_s3_bucket.this.id
+  policy = element(data.template_file.public.*.rendered, 0)
 }
 
 resource "aws_iam_policy" "read" {
-  count       = "${length(var.read_roles) > 0 ? 1 : 0}"
+  count       = length(var.read_roles) > 0 ? 1 : 0
   name        = "${local.id}-S3-Read"
   description = "${var.description} Read"
-  policy      = "${data.aws_iam_policy_document.read.json}"
+  policy      = data.aws_iam_policy_document.read.json
 }
 
 resource "aws_iam_role_policy_attachment" "read" {
-  count      = "${length(var.read_roles)}"
-  role       = "${element(var.read_roles, count.index)}"
-  policy_arn = "${element(aws_iam_policy.read.*.arn, 0)}"
+  count      = length(var.read_roles)
+  role       = element(var.read_roles, count.index)
+  policy_arn = element(aws_iam_policy.read.*.arn, 0)
 }
 
 resource "aws_iam_policy" "write" {
-  count       = "${length(var.write_roles) > 0 ? 1 : 0}"
+  count       = length(var.write_roles) > 0 ? 1 : 0
   name        = "${local.id}-S3-Write"
   description = "${var.description} Write"
-  policy      = "${data.aws_iam_policy_document.write.json}"
+  policy      = data.aws_iam_policy_document.write.json
 }
 
 resource "aws_iam_role_policy_attachment" "write" {
-  count      = "${length(var.write_roles)}"
-  role       = "${element(var.write_roles, count.index)}"
-  policy_arn = "${element(aws_iam_policy.write.*.arn, 0)}"
+  count      = length(var.write_roles)
+  role       = element(var.write_roles, count.index)
+  policy_arn = element(aws_iam_policy.write.*.arn, 0)
 }
